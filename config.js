f448177ffdd1e255d42c8d177064a50e8ab4d181
Jeremy Hotalen <hotalenjeremy@gmail.com>
2021-02-20 11:06:20 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "2826682097586211",
+    clientSecret: "74360da98f3f6cfeb5b9eb490807fcb3",
+  },
 };
